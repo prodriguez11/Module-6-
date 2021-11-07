0470b6ca5a166dc537845fce6ad8a05b36b375c8
prodriguez11 <91973172+prodriguez11@users.noreply.github.com>
2021-11-07 17:17:45 -0600

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,3 +1,9 @@
+#Problem 2
+#import random library 
+#input random command
+#input to print an odd integer between 0-100
+#prints odd integer
+
 import random
 
 print(random.randrange(0,50) * 2 + 1)
