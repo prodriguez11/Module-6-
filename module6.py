bf082995c6e0c45f1fd22c56bfb43ca944e8e338
prodriguez11 <91973172+prodriguez11@users.noreply.github.com>
2021-11-07 17:23:52 -0600

    Update module6.py

--- module6.py
+++ module6.py
@@ -1,4 +1,12 @@
 #Problem 4
+#Import math library 
+#Initialize denominator
+#Initialize sum
+#even index elements are positive
+#odd index elements are negative 
+#denominator is odd
+#print the valuye of pi
+
 import math
 
 k = 1
@@ -11,11 +19,13 @@ for i in range(1000000):
     k += 2
 print("Value of estimated pi:" , s)
 print("Value of pi from math module:", math.pi)
+
 #Problem 5
 rad = float(input("Enter an radians:"))
 degrees = rad * (180/math.pi)
 print("degrees of {} radians using calculation:{}".format(rad,degrees))
 print("degrees of {} radians using inbuilt function:{}".format(rad, math.degrees(rad)))
+
 #Problem 6
 n = int(input("Enter an integer:"))
 result = 1
