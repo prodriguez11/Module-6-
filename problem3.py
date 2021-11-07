0c78f6f77ea71301678b87fc9c7cf35791d17352
prodriguez11 <91973172+prodriguez11@users.noreply.github.com>
2021-11-07 17:19:16 -0600

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -1,3 +1,9 @@
+#Problem 3
+#import random library
+#make list containing days of the week
+#input random command to randomly choose day 
+#prints random choosen day 
+
 import random
 
 days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
